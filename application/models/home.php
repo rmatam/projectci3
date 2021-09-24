7f576876bbed59cbbbad531119b9129fb7298090
rajasekhar <rajasekhar.m@experienceflow.com>
2021-09-24 23:35:23 +0530

    Initial commit

---
+++ application/models/home.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class home extends CI_Model {
+
+    public function index()
+    {
+        echo "This is my model";
+        die();
+    }
+}
